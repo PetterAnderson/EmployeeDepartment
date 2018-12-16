3df233642fed4fe65b37cd15b5e037c4bcc3e690
Petter <petter@bjarred.com>
2018-12-16 21:40:12 +0100

    La till get/set
    La till attribute i klasserna Department och Employee.

--- src/Department.java
+++ src/Department.java
@@ -1,4 +1,19 @@
 
 public class Department {
-
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBudget() {
+		return budget;
+	}
+	public void setBudget(int budget) {
+		this.budget = budget;
+	}
+	private String name;
+	private int budget;
+	
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,21 @@
 
 public class Employee {
 
+	public String getEmpID() {
+		return empID;
+	}
+	public void setEmpID(String empID) {
+		this.empID = empID;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	private String empID;
+	private int salary;
+	
+	
+	
 }
